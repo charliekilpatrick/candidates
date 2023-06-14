25d698c1a49ff388157ad04a93f788740b30fa34
charlie.kilpatrick <ckilpatrick@gmail.com>
2023-06-14 14:56:14 -0700

    Adding example for S190425z

---
+++ examples/s190425z.sh
@@ -0,0 +1,1 @@
+python analysis.py https://ziggy.ucolick.org/yse/explorer/385/download?format=csv --gw-event S190425z --max-date 14 --max-prob 0.95 --redo --steps import prob time astcheck gaia class ps1dr2 asassn redshift
